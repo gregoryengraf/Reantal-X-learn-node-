23d75a47ae447f3347727d89a18f93ed06c24436
Gregory Engraf <gregory0409@gmail.com>
2022-05-21 01:08:09 -0300

    :construction: WIP - Categories

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json("Alohaaaa");
-});
+app.use("/categories", categoriesRoutes);
 
-app.post("courses", (req, res) => {
-  const { name } = req.body;
-  return res.json({ name });
-});
-app.listen(3333, () => console.log("server is running"));
+app.listen(3333, () => console.log("server is running!"));
